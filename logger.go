3ae177f9473574b0f725871c83009b564e1bc5c4
qingconglaixueit <502892037@qq.com>
2023-01-16 16:34:59 +0800

    [add]add code

--- logger.go
+++ logger.go
@@ -16,59 +16,6 @@ type Config struct {
 	Compress   bool
 }
 
-func Info(args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Info(args)
-}
-
-func Infof(template string, args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Infof(template, args)
-}
-
-func Warn(args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Warn(args)
-}
-
-func Warnf(template string, args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Warnf(template, args)
-}
-
-func Error(args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Error(args)
-}
-func Errorf(template string, args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Errorf(template, args)
-}
-func Panic(args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Panic(args)
-}
-func Panicf(template string, args ...interface{}) {
-	if SugarLogger == nil {
-		panic("please init log handler")
-	}
-	SugarLogger.Panicf(template, args)
-}
-
 func InitLog(conf * Config){
 	if conf == nil{
 		panic("please init log handler")
